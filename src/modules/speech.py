c5ee2c2b7bae8a5addda86a28a368d25b7aa4649
haruto2001 <tarako5515@gmail.com>
2024-10-03 01:49:25 +0900

    test: verify functionality of OpenAI's text-to-speech model

---
+++ src/modules/speech.py
@@ -0,0 +1,24 @@
+import os
+
+from openai import OpenAI
+
+
+if __name__ == "__main__":
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    # input_text = (
+    #     "The dominant sequence transduction models are based on complex recurrent or convolutional neural networks in an encoder-decoder configuration."
+    #     "The best performing models also connect the encoder and decoder through an attention mechanism."
+    #     "We propose a new simple network architecture, the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely."
+    #     "Experiments on two machine translation tasks show these models to be superior in quality while being more parallelizable and requiring significantly less time to train."
+    #     "Our model achieves 28.4 BLEU on the WMT 2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU."
+    #     "On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models from the literature."
+    #     "We show that the Transformer generalizes well to other tasks by applying it successfully to English constituency parsing both with large and limited training data."
+    # )
+    input_text = "こんにちは．私はAIです．なにかお手伝いしましょうか？"
+    output_path = "data/test.mp3"
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="alloy",
+        input=input_text,
+    ) as response:
+        response.stream_to_file(output_path)
